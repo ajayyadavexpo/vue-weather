914dac051d8899804e13800205434a0b99103ccb
AjayYadavAi <proajayyadav0525@gmail.com>
2020-06-26 19:24:26 +0530

    Modified all the design

--- src/App.vue
+++ src/App.vue
@@ -7,68 +7,76 @@
         <div class="weather-gradient">
           <div class="date-container">
             <h2 class="date-dayname">Tuesday</h2>
-            <span class="date-day">15 Jan 2019</span>
+            <span class="date-day">{{ toDayDate() }}</span>
             <i class="location-icon" data-feather="map-pin"></i>
-            <span class="location">Paris, FR</span>
-            <div class="weather-container">
-              <i class="weather-icon" data-feather="sun"></i>
-              <h1 class="weather-temp"> 29°C</h1>
-              <h3 class="weather-desc"> Sunny</h3>
+            <span class="location">{{ current.name }}, {{ current.sys.country}}</span>
+          </div>
+          <div class="weather-container">
+            <i class="weather-icon" data-feather="sun"></i>
+            <h1 class="weather-temp"> {{ Math.round(current.main.temp) }}°C</h1>
+            <h3 class="weather-desc"> {{ current.weather[0].main }}</h3>
+          </div>
+
+        </div>
+      </div>
+      <div class="info-side">
+        <div class="today-info-container">
+          <div class="today-info">
+            <div class="precipitation"> 
+              <span class="title"> PRECIPITATION</span>
+              <span class="value">0 %</span>
+              <div class="clear"></div>
+            </div>
+            <div class="humidity">
+              <span class="title"> HUMIDITY</span>
+              <span class="value"> 34 %</span>
+              <div class="clear"></div>
+            </div>
+            <div class="wind">
+              <span class="title"> WIND</span>
+              <span class="value"> 0 km/h</span>
+              <div class="clear"></div>
             </div>
           </div>
         </div>
-          <div class="info-side">
-            <div class="today-info-container">
-              <div class="today-info">
-                <div class="precipitation"> 
-                  <span class="title"> PRECIPITATION</span>
-                  <span class="value">0 %</span>
-                  <div class="clear"></div>
-                </div>
-                <div class="humidity">
-                  <span class="title"> HUMIDITY</span>
-                  <span class="value"> 34 %</span>
-                  <div class="clear"></div>
-                </div>
-                <div class="wind">
-                  <span class="title"> WIND</span>
-                  <span class="value"> 0 km/h</span>
-                  <div class="clear"></div>
-                </div>
-              </div>
-            </div>
-            <div class="week-container">
-              <ul class="week-list">
-                <li class="active">
-                  <i class="day-icon" data-feather="sun"></i>
-                  <span class="day-name"> Tue</span>
-                  <span class="day-temp"> 29°C</span></li>
-                  <li>
-                    <i class="day-icon" data-feather="cloud"></i>
-                    <span class="day-name"> Wed</span>
-                    <span class="day-temp"> 21°C</span></li>
-                    <li>
-                      <i class="day-icon" data-feather="cloud-snow"></i>
-                      <span class="day-name"> Thu</span>
-                      <span class="day-temp"> 08°C</span></li>
-                      <li>
-                        <i class="day-icon" data-feather="cloud-rain"></i>
-                        <span class="day-name"> Fry</span>
-                        <span class="day-temp"> 19°C</span></i>
-                      </li>
-                      <div class="clear"></div>
-                    </ul>
-                  </div>
-                  <div class="location-container">
-                    <button class="location-button">
-                      <i data-feather="map-pin"></i>
-                      <span> Change location </span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div class="week-container">
+          <ul class="week-list">
+            <li class="active">
+              <i class="day-icon" data-feather="sun"></i>
+              <span class="day-name"> Tue</span>
+              <span class="day-temp"> 29°C</span>
+            </li>
+            <li>
+              <i class="day-icon" data-feather="cloud"></i>
+              <span class="day-name"> Wed</span>
+              <span class="day-temp"> 21°C</span>
+            </li>
+            <li>
+              <i class="day-icon" data-feather="cloud-snow"></i>
+              <span class="day-name"> Thu</span>
+              <span class="day-temp"> 08°C</span>
+            </li>
+            <li>
+              <i class="day-icon" data-feather="cloud-rain"></i>
+              <span class="day-name"> Fry</span>
+              <span class="day-temp"> 19°C</span>
+            </li>
+            <div class="clear"></div>
+          </ul>
+        </div>
+
+        <div class="location-container">
+          <button class="location-button">
+            <i data-feather="map-pin"></i>
+            <span> Change location </span>
+          </button>
+        </div>
+
+      </div>
+    </div>
+
+
+
 
 
 
@@ -196,9 +204,7 @@
       },
       toDayDate(){
         var d = new Date();
-        var year = d.getFullYear();
-        var n = d.toDateString()
-        return n.replace(year,"");
+        return d.toDateString()
       }
     }
 
